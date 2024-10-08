37023abe98d285626fde6e12abd405ec87417de2
LearnCodingEasy <learncodingeasy@yahoo.com>
2024-10-08 06:05:29 +0300

    vue press

--- script_youtube_vuepress/docs/.vuepress/.temp/pages/index.html.vue
+++ script_youtube_vuepress/docs/.vuepress/.temp/pages/index.html.vue
@@ -1,4 +1,17 @@
-<template><div><p>This is the content of home page. Check <a href="https://vuejs.press/reference/default-theme/frontmatter.html#home-page" target="_blank" rel="noopener noreferrer">Home Page Docs</a> for more details.</p>
-</div></template>
+<template><div><h2 id="vite-press" tabindex="-1"><a class="header-anchor" href="#vite-press"><span>Vite Press</span></a></h2>
+<div class="language-cmd line-numbers-mode" data-highlighter="prismjs" data-ext="cmd" data-title="cmd"><pre v-pre><code><span class="line">npm init vuepress script_youtube_vuepress</span>
+<span class="line"></span></code></pre>
+<div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0"><div class="line-number"></div></div></div><div class="language-cmd line-numbers-mode" data-highlighter="prismjs" data-ext="cmd" data-title="cmd"><pre v-pre><code><span class="line">cd script_youtube_vuepress</span>
+<span class="line"></span></code></pre>
+<div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0"><div class="line-number"></div></div></div><div class="language-cmd line-numbers-mode" data-highlighter="prismjs" data-ext="cmd" data-title="cmd"><pre v-pre><code><span class="line">npm install -D sass-embedded</span>
+<span class="line"></span></code></pre>
+<div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0"><div class="line-number"></div></div></div><div class="language-cmd line-numbers-mode" data-highlighter="prismjs" data-ext="cmd" data-title="cmd"><pre v-pre><code><span class="line">npm run docs:dev</span>
+<span class="line"></span></code></pre>
+<div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0"><div class="line-number"></div></div></div><ul>
+<li>Open Docs File 📁 &gt; Create File 📁</li>
+</ul>
+<div class="language-text line-numbers-mode" data-highlighter="prismjs" data-ext="text" data-title="text"><pre v-pre><code><span class="line">index.md</span>
+<span class="line"></span></code></pre>
+<div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0"><div class="line-number"></div></div></div></div></template>
 
 
---
+++ script_youtube_vuepress/docs/index.md
@@ -0,0 +1,22 @@
+
+## Vite Press
+
+```cmd
+npm init vuepress script_youtube_vuepress
+```
+
+```cmd
+cd script_youtube_vuepress
+```
+
+```cmd
+npm install -D sass-embedded
+```
+
+```cmd
+npm run docs:dev
+```
+* Open Docs File 📁 > Create File 📁
+```
+index.md
+```
